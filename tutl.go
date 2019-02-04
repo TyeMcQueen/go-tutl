418725c44818a20f4e841fdb8950c889cdbf9226
Tye McQueen <tye.mcqueen@gmail.com>
2019-02-03 17:23:35 -0800

    Allow making u an object rather than an import alias.

--- tutl.go
+++ tutl.go
@@ -44,7 +44,7 @@ type Context struct {
 }
 
 // You can just directly set the maximum line width for single-line test
-// failure output like:
+// failure output [see also TUTL.SetLineWidth()] like:
 //
 //      tutl.Default = 120
 //
@@ -233,6 +233,9 @@ func (c Context) Is(want, got interface {}, desc string, t TestingT) bool {
     }
     line := "Got " + c.S(got) + " not " + c.S(want) + " for " + desc + "."
     wid := utf8.RuneCount([]byte(line))
+    if strings.Contains(line, "\n") {
+        wid = 1 + c.LineWidth
+    }
     if wid <= c.LineWidth-20 {
         t.Error(line)
     } else if wid <= c.LineWidth {
@@ -299,6 +302,9 @@ func (c Context) Like(
                 line := fmt.Sprintf(
                     "No <%s> in <%s> for %s.", m[1:], sgot, desc)
                 wid := utf8.RuneCount([]byte(line))
+                if strings.Contains(line, "\n") {
+                    wid = 1 + c.LineWidth
+                }
                 if wid <= c.LineWidth-20 {
                     t.Error(line)
                 } else if wid <= c.LineWidth {
@@ -315,6 +321,9 @@ func (c Context) Like(
             failed++
             line := fmt.Sprintf("Not like /%s/ in <%s> for %s.", m, sgot, desc)
             wid := utf8.RuneCount([]byte(line))
+            if strings.Contains(line, "\n") {
+                wid = 1 + c.LineWidth
+            }
             if wid <= c.LineWidth-20 {
                 t.Error(line)
             } else if wid <= c.LineWidth {
@@ -346,3 +355,97 @@ func ShowStackOnInterrupt() {
     debug.SetTraceback("all")
     panic("Interrupted")
 }
+
+
+// A type to allow an alternate calling style, especially for Is() and Like().
+type TUTL struct {
+    TestingT
+    c   Context
+}
+
+
+// A unit test can have a huge number of calls to Is().  Having to remember
+// to pass in the *testing.T argument can be inconvenient.  TUTL offers an
+// alternate calling method that replaces the huge number of such extra
+// arguments with a single line of code.  This example code:
+//
+//      import (
+//          "testing"
+//          u "github.com/TyeMcQueen/go-tutl"
+//          ^^  Import alias
+//      )
+//
+//      func TestDur(t *testing.T) {
+//          u.Is("1m 1s", DurationAsText(61), "61", t)
+//          //                                    ^^^  Extra argument
+//          u.Like(Valid("3f", "Error from '3f'", t, "(Unknown|Invalid) unit")
+//          //                                    ^^^  Extra argument
+//      }
+//
+// would become:
+//
+//      import (
+//          "testing"
+//          "github.com/TyeMcQueen/go-tutl"
+//      )
+//
+//      func TestDur(t *testing.T) {
+//          var u = tutl.New(t)
+//          // ^^^^^^^^^^^^^^^^ Added line
+//          u.Is("1m 1s", DurationAsText(61), "61")
+//          u.Like(Valid("3f", "Error from '3f'", "(Unknown|Invalid) unit")
+//      }
+//
+// Whether to use an import alias or New() (or neither) is mostly a personal
+// preference.  Though, using New() also limits the scope of EscapeNewline()
+// MaxLineLine().
+//
+func New(t TestingT) TUTL { return TUTL{t, Default} }
+
+
+// Same as the non-method Is() except the *testing.T argument is held in
+// the TUTL object and so does not need to be passed as an argument.
+//
+func (u TUTL) Is(want, got interface {}, desc string) bool {
+    u.Helper()
+    return u.c.Is(want, got, desc, u)
+}
+
+
+// Same as the non-method Like() except the *testing.T argument is held in
+// the TUTL object and so does not need to be passed as an argument.
+//
+func (u TUTL) Like(got interface {}, desc string, match... string) int {
+    u.Helper()
+    return u.c.Like(got, desc, u, match...)
+}
+
+
+// New() copies the EscapeNewline() state from the Default Conext but future
+// calls to EscapeNewline() only impact one Context, not any other copies.
+//
+func (u *TUTL) EscapeNewline(b bool) { u.c.EscapeNewline(b) }
+
+
+// Same as the non-method S() except that it honors the state from the
+// method version of EscapeNewline() [called on the same object].  New()
+// copies the EscapeNewline() state from the Default Conext but future
+// calls to EscapeNewline() only impact one Context, not any other copies.
+//
+func (u TUTL) S(vs... interface {}) string { return u.c.S(vs...) }
+
+
+// Sets the maximum line width for single-line test failure output, measured
+// in UTF-8 runes.  If either of your values being compared would be displayed
+// with unescaped newlines, then single-line output will not be used.
+//
+func (u *TUTL) SetLineWidth(w int) {
+    u.c.LineWidth = w
+}
+
+
+func (u TUTL) V(v interface {}) string { return V(v) }
+func (u TUTL) DoubleQuote(s string) string { return DoubleQuote(s) }
+func (u TUTL) Escape(r rune) string { return Escape(r) }
+func (u TUTL) Rune(r rune) string { return Rune(r) }
+func (u TUTL) Char(c byte) string { return Char(c) }
